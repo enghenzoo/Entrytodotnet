c30b872d8a46c6ac0219ca6a935b5165b52b3168
HP <HP@LAPTOP-QHQU78QJ>
2025-01-28 22:36:23 +0200

    Add project files.

---
+++ WebApplication1/Controllers/BigController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BigController : Controller
+    {
+        public IActionResult Index()
+        {
+            Company company = new Company {
+                Name = "Best",
+                Id = 01127587911,
+            };
+                
+            Product product = new Product
+            {
+                Name = "Juice",
+                Price = 35,
+                Company = company
+            };
+            return View(product);
+        }
+    }
+}
---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Company Company { get; set; }
+    }
+}
